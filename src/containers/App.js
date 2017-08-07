b07005f6c446bbaeb107f34cc111b8243391dd86
Kachkin Alexander <kachkinsasha@gmail.com>
2017-08-07 18:20:09 +0300

    updated

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
-import {getWeather, removeCity}from "../actions/Actions";
+import * as actions from "../actions/Actions";
 import Cities from "../components/Cities"
 import MenuAdd from "../components/MenuAdd";
 import "../styles/App.sass"
@@ -9,21 +9,21 @@ import "../styles/App.sass"
 class App extends Component{
     handleAddCity(e){
         e.preventDefault();
-        var city = e.target.elements["add-city"].value;
+        let city = e.target.elements["add-city"].value;
         e.target.elements["add-city"].value="";
-        this.props.getWeather(city);
+        this.props.actions.getWeather(city);
     }
     handleRemoveCity(e){
-        var city = e.target.value;
-        this.props.removeCity(city);
+        let city = e.target.value;
+        this.props.actions.removeCity(city);
     }
     render(){
         const {locations}=this.props;
-        //console.log(locations)
+        console.log(this.props);
         return(
             <div>
-                <MenuAdd addCity={this.handleAddCity.bind(this)}/>
-                <Cities removeCity={this.handleRemoveCity.bind(this)} params={locations.params}/>
+                <MenuAdd addCity={(e)=>this.handleAddCity(e)}/>
+                <Cities removeCity={(e)=>this.handleRemoveCity(e)} params={locations.params}/>
             </div>
         )
 
@@ -36,8 +36,7 @@ function State(state) {
 }
 function Dispatch(dispatch) {
     return{
-        getWeather: bindActionCreators(getWeather, dispatch),
-        removeCity: bindActionCreators(removeCity, dispatch)
+        actions: bindActionCreators(actions,dispatch)
     }
 }
 export default connect(State, Dispatch)(App)
